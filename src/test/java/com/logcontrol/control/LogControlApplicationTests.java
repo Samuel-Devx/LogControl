ba917974ca2c03e48ca3922bb797d93defccdecd
Samuel Devx <samuelldeev@gmail.com>
2025-12-03 14:37:26 -0300

    V2 do meu projeto melhorando o seu frontend

--- src/test/java/com/logcontrol/control/LogControlApplicationTests.java
+++ src/test/java/com/logcontrol/control/LogControlApplicationTests.java
@@ -1,13 +1,17 @@
 package com.logcontrol.control;
 
+import com.logcontrol.control.model.Produto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LogControlApplicationTests {
 
+    Produto produto = new Produto("Arroz", 12.0, 100, "SADASD123");
 	@Test
 	void contextLoads() {
+        Assertions.assertEquals("Arroz", produto.getNome());
 	}
 
 }
